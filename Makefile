3023084d9fe9274e78d0db1550109502171058ef
tcnksm <nsd22843@gmail.com>
2016-01-17 16:28:00 +0900

    Initial commit

---
+++ Makefile
@@ -0,0 +1,30 @@
+default: test
+
+updatedeps:
+	go get -v -u ./...
+
+build: deps
+	go build -o bin/goatg
+
+test: 
+	go test -v -parallel 5
+
+test-race:
+	go test -v -race -parallel 5
+
+test-all: vet lint test cover
+
+vet:
+	@go get golang.org/x/tools/cmd/vet
+	go tool vet .
+
+lint:
+	@go get github.com/golang/lint/golint
+	golint ./...
+
+# cover shows test coverages
+cover:
+	@go get golang.org/x/tools/cmd/cover		
+	go test -coverprofile=cover.out
+	go tool cover -html cover.out
+	rm cover.out
---
+++ cli_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestRun_versionFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./gotg -version", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("expected %d to eq %d", status, ExitCodeOK)
+	}
+
+	expected := fmt.Sprintf("gotg version %s", Version)
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("expected %q to eq %q", errStream.String(), expected)
+	}
+}
+
+func TestRun_diffFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./gotg -diff", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
+
+func TestRun_writeFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./gotg -write", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
