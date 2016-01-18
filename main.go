e75e71e3362a25f58118335a976057b342a3dfbb
tcnksm <nsd22843@gmail.com>
2016-01-18 23:32:33 +0900

    Add Debugf function

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+)
+
+const EnvDebug = "DEBUG"
 
 func main() {
 	cli := &CLI{outStream: os.Stdout, errStream: os.Stderr}
 	os.Exit(cli.Run(os.Args))
+}
+
+// Debugf
+func Debugf(format string, v ...interface{}) {
+	if os.Getenv(EnvDebug) != "" {
+		log.Printf("[DEBUG] "+format+"\n", v...)
+	}
 }
