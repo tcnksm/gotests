4a956c0d7a8ea1a8da3587cfa72097269901c087
tcnksm <nsd22843@gmail.com>
2016-01-28 00:31:33 +0900

    Integrate into Emacs

---
+++ editor/emacs/gotests.el
@@ -0,0 +1,21 @@
+;;; gotests.el --- Integration of the gotests into emacs
+
+;; Copyright (C) 2016 Taichi Nakashima
+
+;; Author: Taichi Nakashima
+;; Keywords: go, languages
+
+(defun gotests()
+  (interactive)    
+  (let ((buf (get-buffer-create "*Gotests patch*"))        
+        (coding-system-for-read 'utf-8)
+        (coding-system-for-write 'utf-8))
+    
+    (with-current-buffer buf
+      (erase-buffer))
+
+    ;; Only run when buffer is test file
+    (if (string-match "_test.go" (buffer-file-name))
+        (progn
+          (call-process "gotests" nil buf nil "-w" "-r" (buffer-file-name))
+          (revert-buffer :ignore-auto :noconfirm)))))
